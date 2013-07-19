127b46b40750ca965596173805aa155488e5d690
Scott Vermeire <svermeire@powercoreeng.com>
2013-07-19 14:54:31 -0400

    Separate Data from the headings

--- EDSA_Report_Generator.py
+++ EDSA_Report_Generator.py
@@ -4,10 +4,13 @@ import os
 import csv
 
 """Home Working Directory"""
-os.chdir('c:/Projects/EDSA Report Generator/Test Directory')
+"""os.chdir('c:/Projects/EDSA Report Generator/Test Directory')"""
 
 """PowerCore Working Directory"""
-os.chdir('c:/Documents and Settings/Scott Vermeire/My Documents/Dropbox/EDSA Report Generator/Python Code/Test Directory')
+os.chdir('c:\Documents and Settings\Scott Vermeire\My Documents\Dropbox\EDSA Report Generator\Test Directory')
+
+"""Array Initialization"""
+Data = [0 for x in range(14)]
 
 with open('ARCHEAT.csv') as csvfile:
     iFile = csv.reader(csvfile)
@@ -17,8 +20,11 @@ with open('ARCHEAT.csv') as csvfile:
     for row in iFile:
         if i == 0:
             print('# '.join(row))
-
+            Heading = row
+            print(Heading)
         else:
             print(', '.join(row))
-
+            Data[i] = row
+            print(Data[i])
+            
         i = i+1
