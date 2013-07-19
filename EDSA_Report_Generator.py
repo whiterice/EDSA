7b97fca11ead5aff00f2ed1e9ff236242fcb8991
Scott Vermeire <svermeire6@gmail.com>
2013-07-18 21:20:16 -0400

    Read csv File

--- EDSA_Report_Generator.py
+++ EDSA_Report_Generator.py
@@ -1,3 +1,13 @@
-#EDSA Report Generator
+"""EDSA Report Generator"""
+
 import os
+import csv
+
+"""Temporary Working Directory"""
+
 os.chdir('c:/Projects/EDSA Report Generator/Test Directory')
+
+with open('ARCHEAT.csv') as csvfile:
+     iFile = csv.reader(csvfile)
+     for row in iFile:
+         print(', '.join(row))
