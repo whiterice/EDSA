27894aa58a506d2294febf2327b84e328961e995
Scott Vermeire <svermeire@powercoreeng.com>
2013-09-14 19:56:14 -0400

    Completely Shifted Everything over to operate with the new Class System

--- EDSA_Report_Generator.py
+++ EDSA_Report_Generator.py
@@ -12,118 +12,74 @@ import csv
 #Hard Drive Directory
 os.chdir('f:\Personal Projects\SV0002 - EDSA Report Generator/Test Directory')
 
-# Array Initialization for Reading File
-Data = [0 for x in range(14)]
+# List Declaration
+EquipmentList = []
+Equipment208V = []
+Equipment240V = []
+Equipment600V = []
+Equipment480V = []
+Equipment4160V = []
+EquipmentUNSORTED = []
 
 #Class Declaration
-
 class Equipment:
     'Common Base Class for all Equipment'
-    EquipmentCount=0
+    ECount=0
 
     def __init__(self, BusName, ProtectiveDeviceName, BusVoltage, BoltedFaultCurrent, BranchCurrent, CriticalCase, ArcingCurrent, TripDelayTime, FaultDuration, Configuration, ArcFlashBoundary, WorkingDistance, AvailableEnergy, PPEClass):
-        self.BusName
-        self.ProtectiveDeviceName
-        self.BusVoltage
-        self.BoltedFaultCurrent
-        self.BranchCurrent
-        self.CriticalCase
-        self.ArcingCurrent
-        self.TripDelayTime
-        self.FaultDuration
-        self.Configuration
-        self.ArcFlashBoundary
-        self.WorkingDistance
-        self.AvailableEnergy
-        self.PPEClass
-        Equipment.EquipmentCount += 1
+        self.BusName = BusName
+        self.ProtectiveDeviceName = ProtectiveDeviceName
+        self.BusVoltage = BusVoltage
+        self.BoltedFaultCurrent = BoltedFaultCurrent
+        self.BranchCurrent = BranchCurrent
+        self.CriticalCase = CriticalCase
+        self.ArcingCurrent = ArcingCurrent
+        self.TripDelayTime = TripDelayTime
+        self.FaultDuration = FaultDuration
+        self.Configuration = Configuration
+        self.ArcFlashBoundary = ArcFlashBoundary
+        self.WorkingDistance = WorkingDistance
+        self.AvailableEnergy = AvailableEnergy
+        self.PPEClass = PPEClass
+        Equipment.ECount += 1
+        
+    def DisplayEquipment(self):
+        print("\nBusName :              ", self.BusName, "\nProtectiveDeviceName : ", self.ProtectiveDeviceName, "\nBusVoltage :           ", self.BusVoltage, "\nBoltedFaultCurrent :   ", self.BoltedFaultCurrent, "\nBranchCurrent :        ", self.BranchCurrent, "\nCriticalCase :         ", self.CriticalCase, "\nArcingCurrent :        ", self.ArcingCurrent, "\nTripDelayTime :        ", self.TripDelayTime, "\nFaultDuration :        ", self.FaultDuration, "\nConfiguration :        ", self.Configuration, "\nArcFlashBoundary :     ", self.ArcFlashBoundary, "\nWorkingDistance :      ", self.WorkingDistance, "\nAvailableEnergy :      ", self.AvailableEnergy, "\nPPEClass :             ", self.PPEClass, "\n****************************************\n")
 
-# Split Data from Headings
-with open('ARCHEAT.csv') as csvfile:
-    iFile = csv.reader(csvfile)
+    def DisplayEquipmentCount():
+        print("\nTotal Amount of Equipment Imported : ", Equipment.ECount, "\n****************************************\n")
 
-    i=0
-    for row in iFile:
+# Split Data from Headings and organize into Equipment Class
+i=0
+
+with open('ARCHEAT.csv', newline='') as csvfile:
+    FileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in FileReader:
         if i == 0:
             Heading = row
-            # print(Heading)
         else:
-            Data[i] = row
-            print(Data[i])
-            #print(Data[i][2])
-        i = i+1   
+            EquipmentList.append(Equipment(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13]))
+        i = i+1
 
-"""
-count=1
-while count < i:
-    if Data[count][2] == '0.208':
-        print(Data[count])
+#EquipmentList[0].DisplayEquipment()
+Equipment.DisplayEquipmentCount()
+#print(EquipmentList[0].BusVoltage)
+
+
+
+# Sort Equipment by Voltages
+for EachItem in EquipmentList:
+    if EachItem.BusVoltage=='0.208':
+        Equipment208V.append(Equipment(EachItem.BusName, EachItem.ProtectiveDeviceName, EachItem.BusVoltage, EachItem.BoltedFaultCurrent, EachItem.BranchCurrent, EachItem.CriticalCase, EachItem.ArcingCurrent, EachItem.TripDelayTime, EachItem.FaultDuration, EachItem.Configuration, EachItem.ArcFlashBoundary, EachItem.WorkingDistance, EachItem.AvailableEnergy, EachItem.PPEClass))
+    elif EachItem.BusVoltage=='0.240':
+        Equipment240V.append(Equipment(EachItem.BusName, EachItem.ProtectiveDeviceName, EachItem.BusVoltage, EachItem.BoltedFaultCurrent, EachItem.BranchCurrent, EachItem.CriticalCase, EachItem.ArcingCurrent, EachItem.TripDelayTime, EachItem.FaultDuration, EachItem.Configuration, EachItem.ArcFlashBoundary, EachItem.WorkingDistance, EachItem.AvailableEnergy, EachItem.PPEClass))
+    elif EachItem.BusVoltage=='0.480':
+        Equipment480V.append(Equipment(EachItem.BusName, EachItem.ProtectiveDeviceName, EachItem.BusVoltage, EachItem.BoltedFaultCurrent, EachItem.BranchCurrent, EachItem.CriticalCase, EachItem.ArcingCurrent, EachItem.TripDelayTime, EachItem.FaultDuration, EachItem.Configuration, EachItem.ArcFlashBoundary, EachItem.WorkingDistance, EachItem.AvailableEnergy, EachItem.PPEClass))
+    elif EachItem.BusVoltage=='0.600':
+        Equipment600V.append(Equipment(EachItem.BusName, EachItem.ProtectiveDeviceName, EachItem.BusVoltage, EachItem.BoltedFaultCurrent, EachItem.BranchCurrent, EachItem.CriticalCase, EachItem.ArcingCurrent, EachItem.TripDelayTime, EachItem.FaultDuration, EachItem.Configuration, EachItem.ArcFlashBoundary, EachItem.WorkingDistance, EachItem.AvailableEnergy, EachItem.PPEClass))
+    elif EachItem.BusVoltage=='4.160':
+        Equipment4160V.append(Equipment(EachItem.BusName, EachItem.ProtectiveDeviceName, EachItem.BusVoltage, EachItem.BoltedFaultCurrent, EachItem.BranchCurrent, EachItem.CriticalCase, EachItem.ArcingCurrent, EachItem.TripDelayTime, EachItem.FaultDuration, EachItem.Configuration, EachItem.ArcFlashBoundary, EachItem.WorkingDistance, EachItem.AvailableEnergy, EachItem.PPEClass))
     else:
-        pass
-    count=count+1
-"""
-
-# Initialization for Sorting Lists
-Equipment208V = [0 for x in range(i)]
-Equipment240V = [0 for x in range(i)]
-Equipment600V = [0 for x in range(i)]
-Equipment480V = [0 for x in range(i)]
-Equipment4160V = [0 for x in range(i)]
-EquipmentUNSORTED = [0 for x in range(i)]
-count=1
-j=0
-k=0
-l=0
-m=0
-n=0
-p=0
-
-#List Sorting
-while count < i:
-    if Data[count][2]=='0.208':
-        Equipment208V[j]= Data[count]
-        j=j+1
-    elif Data[count][2]=='0.240':
-        Equipment240V[k]= Data[count]
-        k=k+1
-    elif Data[count][2]=='0.600':
-        Equipment600V[l]= Data[count]
-        l=l+1
-    elif Data[count][2]=='0.480':
-        Equipment480V[m]= Data[count]
-        m=m+1
-    elif Data[count][2]=='4.160':
-        Equipment4160V[n]= Data[count]
-        n=n+1
-    else:
-        EquipmentUNSORTED[p]= Data[count]
-        p=p+1
-    count=count+1
-
-#Resulting List
-if len(Equipment208V)!=0:
-    print('\n\nEquipment208V: \n')
-    for EachObject in Equipment208V:
-        print('\n', EachObject)
-if len(Equipment240V)!=0:
-    print('\n\nEquipment240V: \n')
-    for EachObject in Equipment240V:
-        print('\n', EachObject)
-if len(Equipment600V)!=0:
-    print('\n\nEquipment600V: \n')
-    for EachObject in Equipment600V:
-        print('\n', EachObject)
-if len(Equipment480V)!=0:
-    print('\n\nEquipment480V: \n')
-    for EachObject in Equipment480V:
-        print('\n', EachObject)
-if len(Equipment4160V)!=0:
-    print('\n\nEquipment4160V: \n')
-    for EachObject in Equipment4160V:
-        print('\n', EachObject)
-if len(EquipmentUNSORTED)!=0:
-    print('\n\nEquipmentUNSORTED: \n')
-    for EachObject in EquipmentUNSORTED:
-        print('\n', EachObject)
+        EquipmentUNSORTED.append(Equipment(EachItem.BusName, EachItem.ProtectiveDeviceName, EachItem.BusVoltage, EachItem.BoltedFaultCurrent, EachItem.BranchCurrent, EachItem.CriticalCase, EachItem.ArcingCurrent, EachItem.TripDelayTime, EachItem.FaultDuration, EachItem.Configuration, EachItem.ArcFlashBoundary, EachItem.WorkingDistance, EachItem.AvailableEnergy, EachItem.PPEClass))
 
 
