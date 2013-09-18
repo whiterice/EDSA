a9882817992187f281c3e1c02f6a250355e176c1
Scott Vermeire <svermeire@powercoreeng.com>
2013-09-17 21:23:11 -0400

    Updated to Support Pythong27

--- EDSA_Report_Generator.py
+++ EDSA_Report_Generator.py
@@ -1,7 +1,10 @@
 # EDSA Report Generator
 
+# Imported Modules
 import os
 import csv
+import xlwt
+from datetime import datetime
 
 # Home Working Directory
 #os.chdir('c:/Projects/SV0002 - EDSA Report Generator/Test Directory')
@@ -21,7 +24,6 @@ EquipmentUNSORTED = []
 #Class Declaration
 class Equipment:
     'Common Base Class for all Equipment'
-    ECount=0
 
     def __init__(self, BusName, ProtectiveDeviceName, BusVoltage, BoltedFaultCurrent, BranchCurrent, CriticalCase, ArcingCurrent, TripDelayTime, FaultDuration, Configuration, ArcFlashBoundary, WorkingDistance, AvailableEnergy, PPEClass):
         self.BusName = BusName
@@ -38,18 +40,24 @@ class Equipment:
         self.WorkingDistance = WorkingDistance
         self.AvailableEnergy = AvailableEnergy
         self.PPEClass = PPEClass
-        Equipment.ECount += 1
         
     def DisplayEquipment(self):
-        print("\nBusName :              ", self.BusName, "\nProtectiveDeviceName : ", self.ProtectiveDeviceName, "\nBusVoltage :           ", self.BusVoltage, "\nBoltedFaultCurrent :   ", self.BoltedFaultCurrent, "\nBranchCurrent :        ", self.BranchCurrent, "\nCriticalCase :         ", self.CriticalCase, "\nArcingCurrent :        ", self.ArcingCurrent, "\nTripDelayTime :        ", self.TripDelayTime, "\nFaultDuration :        ", self.FaultDuration, "\nConfiguration :        ", self.Configuration, "\nArcFlashBoundary :     ", self.ArcFlashBoundary, "\nWorkingDistance :      ", self.WorkingDistance, "\nAvailableEnergy :      ", self.AvailableEnergy, "\nPPEClass :             ", self.PPEClass, "\n****************************************\n")
+        print '\nBusName :              ', self.BusName, '\nProtectiveDeviceName : ', self.ProtectiveDeviceName, '\nBusVoltage :           ', self.BusVoltage, '\nBoltedFaultCurrent :   ', self.BoltedFaultCurrent, '\nBranchCurrent :        ', self.BranchCurrent, '\nCriticalCase :         ', self.CriticalCase, '\nArcingCurrent :        ', self.ArcingCurrent, '\nTripDelayTime :        ', self.TripDelayTime, '\nFaultDuration :        ', self.FaultDuration, '\nConfiguration :        ', self.Configuration, '\nArcFlashBoundary :     ', self.ArcFlashBoundary, '\nWorkingDistance :      ', self.WorkingDistance, '\nAvailableEnergy :      ', self.AvailableEnergy, '\nPPEClass :             ', self.PPEClass, '\n****************************************\n'
 
-    def DisplayEquipmentCount():
-        print("\nTotal Amount of Equipment Imported : ", Equipment.ECount, "\n****************************************\n")
+# Styles for Excel Report
+TableText_Style = xlwt.easyxf('pattern: pattern solid, fore_colour white; font: height 200, name Arial, color-index black; border: left 2, right 2, top 2, bottom 2', num_format_str='#,##0.00')
+Archeat0_Style = xlwt.easyxf('pattern: pattern solid, fore_colour green; font: height 200, name Arial, color-index black; border: left 2, right 2, top 2, bottom 2', num_format_str='#,##0.00')
+Archeat1_Style = xlwt.easyxf('pattern: pattern solid, fore_colour yellow; font: height 200, name Arial, color-index black; border: left 2, right 2, top 2, bottom 2', num_format_str='#,##0.00')
+Archeat2_Style = xlwt.easyxf('pattern: pattern solid, fore_colour orange; font: height 200, name Arial, color-index black; border: left 2, right 2, top 2, bottom 2', num_format_str='#,##0.00')
+Archeat3_Style = xlwt.easyxf('pattern: pattern solid, fore_colour pink; font: height 200, name Arial, color-index black; border: left 2, right 2, top 2, bottom 2', num_format_str='#,##0.00')
+Archeat4_Style = xlwt.easyxf('pattern: pattern solid, fore_colour red; font: height 200, name Arial, color-index black; border: left 2, right 2, top 2, bottom 2', num_format_str='#,##0.00')
+ArcheatNA_Style = xlwt.easyxf('pattern: pattern solid, fore_colour brown; font: height 200, name Arial, color-index black; border: left 2, right 2, top 2, bottom 2', num_format_str='#,##0.00')
+
 
 # Split Data from Headings and organize into Equipment Class
 i=0
 
-with open('ARCHEAT.csv', newline='') as csvfile:
+with open('ARCHEAT.csv') as csvfile:
     FileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in FileReader:
         if i == 0:
@@ -59,7 +67,6 @@ with open('ARCHEAT.csv', newline='') as csvfile:
         i = i+1
 
 #EquipmentList[0].DisplayEquipment()
-Equipment.DisplayEquipmentCount()
 #print(EquipmentList[0].BusVoltage)
 
 
