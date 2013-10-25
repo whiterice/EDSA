3c7d41e61cc180550665573b13478a2b8188eff0
Scott Vermeire <svermeire@powercoreeng.com>
2013-10-25 13:16:52 -0400

    remade GUI to a command line style; still required name Variable Editing, Archeat hunting and Address Hunting

--- AF_GUI.py
+++ AF_GUI.py
@@ -1,41 +1,77 @@
 import EDSA_Report_Generator
 import os
 
-GUI_dir=os.getcwd()
-
 print "****************************\n*Arc Flash Report Generator*\n****************************"
-print "Instructions:\nOnly Archeat Tables Available. Arc Flash Ressults must be saved under 'ARCHEAT.csv'"
 
-cont = 'y'
-while (cont != 'quit'):
-    try:
-        print "Please Specify where you Archeat Data is:"
-        Working_Directory = str(raw_input())
+GUI_dir=os.getcwd()
+os.chdir('../Test Directory/S2777-City Of London Arc Flash 2013')
+Available_Options=os.listdir(os.getcwd())
 
-        print "Job Number:"
-        Job_Number = str(raw_input())
+Full_Path = os.getcwd()
+Full_Path_List = Full_Path.split('\\')
 
-        print "Company Owning Site Under Investigation:"
-        Customer_Company = str(raw_input())
+Main_dir = Full_Path_List[len(Full_Path_List)-1]
 
-        print "Name Of Site Under Investigation:"
-        Customer_Building = str(raw_input())
+print '\n{!s}:'.format(Main_dir)
 
-        print "Address Of Site Under Investigation:"
-        Customer_Address = str(raw_input())
+Available_dir =[]
+for dir_i in Available_Options:
+    if os.path.isdir(dir_i):
+        Available_dir.append(dir_i)
 
-        print '\n'
+for i,v in enumerate(Available_dir):
+    print "{:d}) {!s}".format(i,v)
 
-        """Runs the Tap Changer Simulation Through TapChanger.py"""
-        ArcheatTable(Job_Number, Customer_Company, Customer_Building, Customer_Address,
-                     Working_Directory)
+print '\nSelect directory using the associated integer:'
+Available_dir_index = int(raw_input())
 
-    except:
-        print "\nInavlid Input\n"
-    
-    print "\nWant to Run another Scneario?(y/n)"
-    cont = raw_input()
+w_dir = Available_dir[Available_dir_index]
 
-    print "\n"
+print '\nYou Have Selected: {!s}'.format(w_dir)
+os.chdir(w_dir)
+
+
+
+if w_dir.find('-')!=-1:
+    temp = w_dir.split('-')
+    Job_Number = temp[0]
+else:
+    Job_Number = '<Unknown>'
+
+if Main_dir.find('-')!=-1:
+    temp = Main_dir.split('-')
+    Customer_Company = temp[1]
+else:
+    Customer_Company = '<Unknown>'
+
+if w_dir.find('-')!=-1:
+    temp = w_dir.split('-')
+    Customer_Building = temp[len(temp)-1]
+else:
+    Customer_Building = '<Unknown>'        
+ 
+
+Customer_Address = '<Unknown>'
+
+Working_Directory = os.getcwd()
+
+resp = 'n'
+while resp!='y':
+    Arguments_Required = ['Job_Number: {!s}'.format(Job_Number),
+                          'Customer_Company: {!s}'.format(Customer_Company),
+                          'Customer_Building: {!s}'.format(Customer_Building),
+                          'Customer_Address: {!s}'.format(Customer_Address),
+                          'Working_Directory: {!s}'.format(Working_Directory)]
+
+    print '\nDetected Info:'
+    for i,v in enumerate(Arguments_Required):
+        print "{:d}) {!s}".format(i,v)
+
+    print '\nIs This Correct?'
+    resp = str(raw_input())
+
+#os.chdir(GUI_dir)
+EDSA_Report_Generator.ArcheatTable(Job_Number, Customer_Company, Customer_Building, Customer_Address,
+                                    Working_Directory)
 
 exit()
